0bfb36650555923ad13778582100886f5b50ceaa
MaetamongVH <lvt11072000@gmail.com>
2020-09-01 16:01:17 +0700

    change deploy to glitch

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //@ts-nocheck
+
 const express = require("express");
 
 const cors = require("cors");
-const puppeteer = require("puppeteer");
 
 const app = express();
 
@@ -14,6 +14,8 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/controllers"));
 
 const { downloadImage } = require("./controllers/download.js");
+
+const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
@@ -28,8 +30,6 @@ app.get("/", (req, res) => {
 app.post("/img", async (req, res) => {
   downloadImage(req, res);
 });
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
 });
